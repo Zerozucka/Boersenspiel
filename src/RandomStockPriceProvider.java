1624fbbb4d07a9fe90bc0b4d4276b546abf4ea47
PhilippSe <se.philipp.835@gmail.com>
2013-04-16 17:37:02 +0300

    Update RandomStockPriceProvider.java
    implementation RandomStockPriceProvider

--- src/RandomStockPriceProvider.java
+++ src/RandomStockPriceProvider.java
@@ -2,15 +2,30 @@ import animalpackage.Share;
 import java.util.Random;
 
 public class RandomStockPriceProvider extends StockPriceProvider {
+    
+	Random randomGenerator = new java.util.Random();
+	
     public RandomStockPriceProvider() {
 
     }
 
+    /**
+     * updateShareRate(Share share)
+     * 		sucht in der shareArray nach der übergebenen Share
+     * 		ersetzt den Aktuellen Kurs einer übergebenen Share durch einen Zufallswert.
+     */
     public void updateShareRate(Share share) {
     	Share foundShare = super.findShare(share.getName());
+    	foundShare.setRate(randomGenerator.nextInt(1000));
     }
     
-    public void updateShareRates(){
-    	
+    /**
+     * updateShareRates()
+     * 		ersetzt alle Share Werte in der shareArray durch Zufallszahlen < 1000.
+     */
+    public void updateShareRates() {
+    	Share[] shareArray = super.getShareArray();
+    	for(int c = 0; c < shareArray.length; c++)
+    		shareArray[c].setRate(randomGenerator.nextInt(1000));	
     }
 }
