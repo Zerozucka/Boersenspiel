8d96822ae9e1e9abd7cdad201e3f4926b4365453
michimogk <michi@mogele.de>
2013-04-18 11:07:08 +0200

    fixed Issue PlayerNotFound

--- src/CommandProcessor.java
+++ src/CommandProcessor.java
@@ -21,7 +21,8 @@ public class CommandProcessor {
                 }
 
                 try {
-                    System.out.println("Spieler konnte nicht gefunden werden, soll ein neuer Spieler erstellt werden? (ja/nein)");
+                    System.out
+                            .println("Spieler konnte nicht gefunden werden, soll ein neuer Spieler erstellt werden? (ja/nein)");
                     String abfrage = System.console().readLine("> ");
                     switch (abfrage) {
                         case "ja":
@@ -41,86 +42,91 @@ public class CommandProcessor {
                     System.out.println("Spieler gefunden, Login erfolgt!\n");
                 }
                 currentPlayer = playerName;
-            }
+            } else {
 
-            String s = System.console().readLine("> ");
+                String s = System.console().readLine("> ");
 
-            switch (s) {
-                case ("buy"): {
-                    System.out.println("Name der zu kaufenden Aktien eingeben:");
-                    String shareName = System.console().readLine("> ");
+                switch (s) {
+                    case ("buy"): {
+                        System.out.println("Name der zu kaufenden Aktien eingeben:");
+                        String shareName = System.console().readLine("> ");
 
-                    System.out.println("Wie viele Aktien sollen gekauft werden?");
-                    String amountInput = System.console().readLine("> ");
+                        System.out.println("Wie viele Aktien sollen gekauft werden?");
+                        String amountInput = System.console().readLine("> ");
 
-                    try {
-                        long amount = Integer.parseInt(amountInput);
-                        acc.buyShare(currentPlayer, shareName, amount);
-                        System.out.println("Kauf der Aktie " + shareName + " erfolgreich\n");
-                    } catch (NotEnoughMoneyException e) {
-                        System.out.println("Nicht ausreichend Geld vorhanden!\n");
-                    } catch (NumberFormatException e) {
-                        System.out.println("Keine Zahl eingegeben!\n");
-                    }
-                    break;
-                }
-
-                case ("sell"): {
-                    System.out.println("Name der zu verkaufenden Aktien eingeben");
-                    String shareName = System.console().readLine("> ");
-
-                    System.out.println("Menge Eingeben");
-                    String amountInput = System.console().readLine("> ");
-
-                    try {
-                        long amount = Integer.parseInt(amountInput);
-                        acc.sellShare(currentPlayer, shareName, amount);
-                        System.out.println("Verkauf der Aktie " + shareName + " erfolgreich");
-                    } catch (NumberFormatException e) {
-                        System.out.println("Keine Zahl eingegeben!\n");
+                        try {
+                            long amount = Integer.parseInt(amountInput);
+                            acc.buyShare(currentPlayer, shareName, amount);
+                            System.out.println("Kauf der Aktie " + shareName + " erfolgreich\n");
+                        } catch (NotEnoughMoneyException e) {
+                            System.out.println("Nicht ausreichend Geld vorhanden!\n");
+                        } catch (NumberFormatException e) {
+                            System.out.println("Keine Zahl eingegeben!\n");
+                        }
+                        break;
                     }
 
-                    break;
+                    case ("sell"): {
+                        System.out.println("Name der zu verkaufenden Aktien eingeben");
+                        String shareName = System.console().readLine("> ");
+
+                        System.out.println("Menge Eingeben");
+                        String amountInput = System.console().readLine("> ");
+
+                        try {
+                            long amount = Integer.parseInt(amountInput);
+                            acc.sellShare(currentPlayer, shareName, amount);
+                            System.out.println("Verkauf der Aktie " + shareName + " erfolgreich");
+                        } catch (NumberFormatException e) {
+                            System.out.println("Keine Zahl eingegeben!\n");
+                        }
+
+                        break;
+                    }
+
+                    case ("cash"):
+                        System.out.println("Aktuelles Guthaben: " + acc.getCashAccountValue(currentPlayer) + " Euro\n");
+                        break;
+
+                    case ("depovalue"):
+                        System.out.println("Aktuelles Wert des Depots: " + acc.getDepositValue(currentPlayer)
+                                + " Euro\n");
+                        break;
+
+                    case ("logout"):
+                        currentPlayer = null;
+                        System.out.println("Logout erfolgreich\n");
+                        break;
+
+                    case ("assetvalue"):
+                        System.out.println("Aktueller Wert aller Vermoegenswerte " + acc.getAssetValue(currentPlayer)
+                                + " Euro\n");
+                        break;
+
+                    case ("quit"):
+                        System.out.println("Goodbye");
+                        System.exit(0);
+                        break;
+                    case ("help"): {
+                        String commands = "buy                  --- Kauft Aktien\n"
+                                + "sell                 --- Verkauft Aktien\n"
+                                + "cash                 --- Zeigt das aktuelle Guthaben an\n"
+                                + "depovalue            --- Zeigt den aktuellen Wert aller gekauften Aktien an\n"
+                                + "assetvalue           --- Zeigt das gesamte Vermoegen an\n"
+                                + "logout               --- Meldet den derzeit angemeldeten Nutzer ab\n"
+                                + "quit                 --- Beendet das Programm\n";
+
+                        System.out.println(commands);
+                        break;
+                    }
+
+                    default:
+                        System.out
+                                .println("Der Befehl "
+                                        + s
+                                        + " ist falsch geschrieben oder konnte nicht erkannt werden.\nGeben Sie <help> fuer eine Liste aller Befehle ein.\nGeben Sie <quit> zum Beenden ein.\n");
+                        break;
                 }
-
-                case ("cash"):
-                    System.out.println("Aktuelles Guthaben: " + acc.getCashAccountValue(currentPlayer) + " Euro\n");
-                    break;
-
-                case ("depovalue"):
-                    System.out.println("Aktuelles Wert des Depots: " + acc.getDepositValue(currentPlayer) + " Euro\n");
-                    break;
-
-                case ("logout"):
-                    currentPlayer = null;
-                    System.out.println("Logout erfolgreich\n");
-                    break;
-
-                case ("assetvalue"):
-                    System.out.println("Aktueller Wert aller Vermoegenswerte " + acc.getAssetValue(currentPlayer) + " Euro\n");
-                    break;
-
-                case ("quit"):
-                    System.out.println("Goodbye");
-                    System.exit(0);
-                    break;
-                case ("help"): {
-                    String commands = "buy                  --- Kauft Aktien\n"
-                            + "sell                 --- Verkauft Aktien\n"
-                            + "cash                 --- Zeigt das aktuelle Guthaben an\n"
-                            + "depovalue            --- Zeigt den aktuellen Wert aller gekauften Aktien an\n"
-                            + "assetvalue           --- Zeigt das gesamte Vermoegen an\n"
-                            + "logout               --- Meldet den derzeit angemeldeten Nutzer ab\n"
-                            + "quit                 --- Beendet das Programm\n";
-
-                    System.out.println(commands);
-                    break;
-                }
-
-                default:
-                    System.out.println("Der Befehl " + s + " ist falsch geschrieben oder konnte nicht erkannt werden.\nGeben Sie <help> fuer eine Liste aller Befehle ein.\nGeben Sie <quit> zum Beenden ein.\n");
-                    break;
-
             }
         }
     }
